0964497445f0a620c0c93a1d3e038ffe152d732b
Kislay Mishra <78745590+KislayMishra69@users.noreply.github.com>
2025-10-04 12:23:07 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const filePath = 'todos.json';
+
+let todos = [];
+
+// Load tasks from the JSON file
+function loadTodos() {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    todos = JSON.parse(data);
+  }
+}
+
+function saveTodos() {
+  fs.writeFileSync(filePath, JSON.stringify(todos, null, 2));
+}
+
+// Add a new task
+function addTodo(task) {
+  todos.push(task);
+  saveTodos();
+}
+
+// List all tasks
+function listTodos() {
+  if (todos.length === 0) {
+    console.log('No tasks yet!');
+    return;
+  }
+  todos.forEach((task, index) => {
+    console.log(`${index}: ${task}`);
+  });
+}
+
+// Remove a task by index
+function removeTodo(index) {
+  if (index >= 0 && index < todos.length) {
+    todos.splice(index, 1);
+    saveTodos();
+  } else {
+    console.log('Invalid task index.');
+  }
+}
+
+// Initialize tasks on startup
+loadTodos();
+
+module.exports = { addTodo, listTodos, removeTodo };
